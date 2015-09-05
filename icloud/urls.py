73c63caa6cafaa4c23841a335e469dcc44eccc60
Langley <295046974@qq.com>
2015-09-05 12:00:09 +0800

    qiniu v6 sdk

--- icloud/urls.py
+++ icloud/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^callback$', 'ifile.views.upload_callback'),
+    url(r'^callback', 'ifile.views.upload_callback'),
 )

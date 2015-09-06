0e6cb36cf7f9b516b22545cd3d0f95cce7971e44
Langley <295046974@qq.com>
2015-09-06 14:24:38 +0800

    qiniu v6 sdk

--- ifile/config.py
+++ ifile/config.py
@@ -4,7 +4,7 @@ ACCESS_KEY = 'YUS5eY4T8fP4Q8MgeLSPVmeej-KppcJwQlmQPR29'
 SECRET_KEY = 'hx22Fa6jJrdHUg0IWrY8L2TK5WyQ9WR_d3VqUPpq'
 
 BUCKET_NAME = 'ifile'
-Q_DOMAIN = '7xlk5b.com1.z0.glb.clouddn.com' # your qiniu domain
+Q_DOMAIN = 'ifile.icecoffee.cn' # your qiniu domain
 
 CALLBACK_URL = 'http://lab.icecoffee.cn/callback' # explained in http://docs.qiniu.com/api/v6/put.html#put-policy
 
